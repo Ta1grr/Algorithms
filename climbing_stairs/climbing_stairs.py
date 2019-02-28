8622a973d141aadf95dc2cde447c46a16e30d3ad
Ta1grr <taile602@gmail.com>
2019-02-28 17:09:49 -0600

    Still working, doing most of my work in repl.it before commiting it

--- climbing_stairs/climbing_stairs.py
+++ climbing_stairs/climbing_stairs.py
@@ -2,8 +2,39 @@
 
 import sys
 
-def climbing_stairs(n, cache=None):
-  pass 
+def climbing_stairs(n, cache={}):
+  result = []
+  print("n:", n)
+  if n in cache:
+    return cache[n]
+  elif n > 1:
+    return cache.setdefault(n, climbing_stairs(n - 1) + climbing_stairs(n - 2) + climbing_stairs(n - 3))
+  return n
+
+
+# 4
+# 1 + 1 + 1 + 1
+# 1 + 2 + 1
+# 2 + 1 + 1
+# 3 + 1
+# 1 + 3
+# 1 + 1 + 2
+# 2 + 2
+
+# 5
+# 2 + 2 + 1
+# 1 + 2 + 2
+# 1 + 1 + 1 + 1 + 1
+# 2 + 1 + 2
+# 1 + 2 + 1 + 1
+# 2 + 1 + 1 + 1
+# 1 + 1 + 2 + 1
+# 1 + 1 + 1 + 2
+# 3 + 2
+# 2 + 3
+# 3 + 1 + 1
+# 1 + 3 + 1
+# 1 + 1 + 3
 
 
 if __name__ == "__main__":
